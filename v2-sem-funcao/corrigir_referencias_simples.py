397d5294e0cbd0509c2cebe7abc99c0a8f884053
Cadu <141927294+cadudipek@users.noreply.github.com>
2025-02-19 15:55:47 -0300

    Update corrigir_referencias_simples.py

--- v2-sem-funcao/corrigir_referencias_simples.py
+++ v2-sem-funcao/corrigir_referencias_simples.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv('sua_planilha.csv', sep=';')
+df['Referencia'] = df['Referencia'].apply(lambda x: x[:-3] if str(x).endswith('.03') else x)
+df.to_csv('planilha_atualizada.csv', sep=';', index=False)
+
+print("Arquivo atualizado com sucesso!")
 
